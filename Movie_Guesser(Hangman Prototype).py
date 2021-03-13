81acbb4094536e6aa0f1894a6e8d9748af04d146
nullbite-coder <63859510+nullbite-coder@users.noreply.github.com>
2021-03-13 16:16:30 +0530

    Update Movie_Guesser(Hangman Prototype).py

--- Movie_Guesser(Hangman Prototype).py
+++ Movie_Guesser(Hangman Prototype).py
@@ -32,17 +32,24 @@ print(guess)
 while(attempts > 0 and win == 1):
     
     print(letters)
-    print("Attempts Left - ",attempts)
+    print("\n\nAttempts Left - ",attempts)
     user_input = input("Enter Your Guess - ")
     try:
         if user_input.upper() in letters:
             letters.remove(user_input.upper())
+            flag = 0
+        elif user_input == '':
+            raise EOFError
         else:
-            raise ValueError("Already Choosen this Value")
+            raise ValueError
     except ValueError as err:
         print("You have already choosen this value, Choose different Value")
+        flag = 1
+    except EOFError:
+        print("Value is Null")
+        flag =1
     
-    flag = 0
+    
     for i in range(len(movies)):
         if(user_input.upper() == movies[i]):
             guess[i] = user_input.upper()
@@ -55,8 +62,9 @@ while(attempts > 0 and win == 1):
         attempts-=1
     
     print(guess)
+    print("\n")
 
     win = check(guess)
 
 name  = movie_name(guess)
-print(name)]
+print(name)
